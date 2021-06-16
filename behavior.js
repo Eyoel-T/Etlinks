195de8942d5e09dacfdac4589671791be723fafc
Eyoel-T <71425416+Eyoel-T@users.noreply.github.com>
2021-06-16 10:22:09 +0300

    Update behavior.js

--- behavior.js
+++ behavior.js
@@ -1,1 +1,7 @@
 
+(function() { // DON'T EDIT BELOW THIS LINE
+    var d = document, s = d.createElement('script');
+    s.src = 'https://et-links-com.disqus.com/embed.js';
+    s.setAttribute('data-timestamp', +new Date());
+    (d.head || d.body).appendChild(s);
+    })();
